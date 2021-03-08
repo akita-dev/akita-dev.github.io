46cd2effc6a63abb5ea50922373a22679944bf10
ghost <10137+ghost@users.noreply.github.com>
2021-03-08 14:47:44 +0800

    deploy

---
+++ precache-manifest.99d25dae9a137737ccd832257a0ffd5d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0baa3e1b9006a2afe32bdd3cc3853ba",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "57dc0f6f2bad4eaa879a93768969151d",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "ad1dea1d2ca147495e3d98e1b4ff9aa6",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "5e75893e28648328b9b4",
+    "url": "/css/app.7f2cf8a5.css"
+  },
+  {
+    "revision": "a5628724022dbaf873c61768cd50ef08",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "db09ce8a5a6ecb22a0ccb903cea7f88e",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "5cc78af1f65403e044a571c7935c6ba5",
+    "url": "/img/alpaca.5cc78af1.png"
+  },
+  {
+    "revision": "132496e91a7ffc436d60200888f49b34",
+    "url": "/img/bigAkita.132496e9.svg"
+  },
+  {
+    "revision": "8756939144def62f9ef3fa58f7459331",
+    "url": "/img/busd.87569391.png"
+  },
+  {
+    "revision": "d1ae73e20f994d0445773750d371140c",
+    "url": "/img/logo.d1ae73e2.svg"
+  },
+  {
+    "revision": "350b01f034d1009ba3275e7997ea8878",
+    "url": "/img/logo512.350b01f0.png"
+  },
+  {
+    "revision": "7c25ec20b0a14cd28bbe552f230b7ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e75893e28648328b9b4",
+    "url": "/js/app.055c4eda.js"
+  },
+  {
+    "revision": "da4a9610f493c77be3a5",
+    "url": "/js/chunk-2d0ddb73.15cd3a70.js"
+  },
+  {
+    "revision": "b7a61e2b6294210ad223",
+    "url": "/js/chunk-vendors.32211e2b.js"
+  },
+  {
+    "revision": "52e2f0c8f468ff6a8528e7afd597f209",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f4605d23e789fa9c8ac194ae38ea38cf",
+    "url": "/mstile-144x144.png"
+  },
+  {
+    "revision": "832803c6dffa2f05a6bf11dc83a1f7b0",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "494dc4b35ff2ae820baa448b6f56446a",
+    "url": "/mstile-310x150.png"
+  },
+  {
+    "revision": "f192907d75b3db2e383ec11bd5f6c23b",
+    "url": "/mstile-310x310.png"
+  },
+  {
+    "revision": "203c0372054f0ad00a0462a59ed1bfda",
+    "url": "/mstile-70x70.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d2740be25613f52f35f47e26f99b55d5",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/site.webmanifest"
+  }
+]);
